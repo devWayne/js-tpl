995e763406a16c01c692a3243fe8854557115b3f
Wayne <dev.wayne.ix@gmail.com>
2015-03-01 11:42:45 +0800

    reg

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function jsTpl(){
+function jsTpl() {
+    var compiled = 'var o="";',
+        reader;
+    while ((reader = REGEX_JSTL_SCOPE.exec(template)) !== null) {
 
-  return new Function('it', compiled);
+    }
+    return new Function('it', compiled);
 }
 
-exports.render = function(template, data){
+
+var REGEX_JSTL_SCOPE = /<\?js((?:.|\r|\n)+?)\?>|@\{(.+?)\}/g; // lazy match
+var CODE_PRE = '<?js';
+var PARAM_PRE = '@{';
+
+
+exports.render = function(template, data) {
     return jsTpl(template)(data);
 };
 
-exports.compile = function(template){
+exports.compile = function(template) {
     return jsTpl(template);
 };
