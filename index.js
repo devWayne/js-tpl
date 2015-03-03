d3c6889c1a09f105a175b7f873e93201517cad3b
Wayne <dev.wayne.ix@gmail.com>
2015-03-03 11:20:56 +0800

    gobal & commonjs support

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
-function jsTpl() {
-    var compiled = 'var o="";',
-        reader;
-    while ((reader = REGEX_JSTL_SCOPE.exec(template)) !== null) {
-
+(function(global, factory) {
+    if (typeof exports === "object" && exports) {
+        factory(exports); // CommonJS
+    }  else {
+        factory(global.jsTpl = {}); // <script>
     }
-    return new Function('it', compiled);
-}
+}(this, function(jsTpl) {
+
+    function fGen(template) {
+        var compiled = 'var o="";',
+	    last = 'begin',
+            reader;
+        while ((reader = REGEX_JSTL_SCOPE.exec(template)) !== null) {
+
+        }
+
+        compiled += COMPILERS[last].end + 'return o;';
+        return new Function('it', compiled);
+    }
 
 
-var REGEX_JSTL_SCOPE = /<\?js((?:.|\r|\n)+?)\?>|@\{(.+?)\}/g; // lazy match
-var CODE_PRE = '<?js';
-var PARAM_PRE = '@{';
+    var REGEX_JSTL_SCOPE = /<\?js((?:.|\r|\n)+?)\?>|@\{(.+?)\}/g; // lazy match
+    var CODE_PRE = '<?js';
+    var PARAM_PRE = '@{';
 
 
-exports.render = function(template, data) {
-    return jsTpl(template)(data);
-};
+    var COMPILERS = {
+        begin: {
+            // the end of the matched code slice
+            addString: 'o+="',
+            addCode: '',
+            addParam: 'o+=',
+            end: ''
+        },
 
-exports.compile = function(template) {
-    return jsTpl(template);
-};
+        // string between code snippets and variables
+        string: {
+            addString: '',
+            addCode: '";',
+            addParam: '"+',
+            end: '";'
+        },
+
+        // JavaScript code between `<?js` and its corresponding `?>`
+        code: {
+            addString: 'o+="',
+            addCode: '',
+            addParam: 'o+=',
+            end: ''
+        },
+
+        // JavaScript variable between `@{` and `}`
+        param: {
+            addString: '+"',
+            addCode: ';',
+            addParam: '+',
+            end: ';'
+        }
+    };
+
+
+
+    jsTpl.render = function(template, data) {
+        return fGen(template)(data);
+    };
+
+    jsTpl.compile = function(template) {
+        return fGen(template);
+    };
+
+}));
